9302a3d95e92710103486332d9ae4bbaa1c85e46
ZhongWen <lizw@primeton.com>
2020-05-25 11:34:57 +0800

    SpringBoot

--- src/main/java/org/javaee/web/hello/HelloApplication.java
+++ src/main/java/org/javaee/web/hello/HelloApplication.java
@@ -3,7 +3,9 @@ package org.javaee.web.hello;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.core.SpringProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jndi.JndiLocatorDelegate;
 
 @SpringBootApplication
 @EntityScan({"org.javaee.web.hello.entity"})
@@ -11,6 +13,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class HelloApplication {
 
 	public static void main(String[] args) {
+		SpringProperties.setFlag(JndiLocatorDelegate.IGNORE_JNDI_PROPERTY_NAME);
+		
 		SpringApplication.run(HelloApplication.class, args);
 	}
 
